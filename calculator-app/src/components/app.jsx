e2c77a0538c981911e927a6b6df8b87c43d14b5f
Yang Zhou <yangzhou12138@outlook.com>
2022-07-26 17:20:57 +0800

    2022-7-26 v3
    completed v1

--- calculator-app/src/components/app.jsx
+++ calculator-app/src/components/app.jsx
@@ -6,20 +6,39 @@ import Calculator from './content/calculator';
 import Login from './content/login';
 import Register from './content/register';
 import NotFound from './content/notFound';
+//import $ from 'jquery'
 
 class App extends Component {
-    state = {  } 
+    state = { 
+        is_login: true,
+        username:"Yang",
+     } 
+
+     // judge whether the user is in login status —— after the completion of component loaded
+    //  componentDidMount() {
+    //     $.ajax({
+    //         url: "https://app165.acapp.acwing.com.cn/calculator/get_status/",
+    //         type: "get",
+    //         success: resp => {
+    //             if(resp.result === "login")
+    //                 this.setState({is_login: true, username: resp.username})
+    //             else
+    //                 this.setState({is_login: false})
+    //         }
+    //     })
+    //  }
+
     render() { 
         return (
             <React.Fragment>
-                <NavBar />
+                <NavBar is_login={this.state.is_login} username={this.state.username}/>
                 <div className='container'>
                     <Routes>
                         <Route path='/calculator' element={<Home />}></Route>
                         <Route path='/calculator/home' element={<Home />}></Route>
-                        <Route path='/calculator/calculator' element={<Calculator />}></Route>
-                        <Route path='/calculator/login' element={<Login />}></Route>
-                        <Route path='/calculator/register' element={<Register />}></Route>
+                        <Route path='/calculator/calculator' element={this.state.is_login ? <Calculator /> : <Navigate replace to='/calculator/login' />}></Route>
+                        <Route path='/calculator/login' element={this.state.is_login ? <Navigate replace to='/calculator' /> : <Login />}></Route>
+                        <Route path='/calculator/register' element={this.state.is_login ? <Navigate replace to='/calculator' />:<Register />}></Route>
                         <Route path='/calculator/404' element={<NotFound />}></Route>
                         <Route path='/calculator/*' element={<Navigate replace to='/calculator/404' />}></Route>
                     </Routes>
--- calculator-app/src/components/navbar.jsx
+++ calculator-app/src/components/navbar.jsx
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import $ from 'jquery'
 
 class NavBar extends Component {
     state = {  } 
+
+    handleClick = () => {
+        $.ajax({
+            url: "https://app165.acapp.acwing.com.cn/calculator/logout/",
+            type: "get",
+            success: resp => {
+                if(resp.result === "success")
+                    window.location.href = "/calculator";
+            }
+        })
+    }
+
+    render_calculator = () =>{
+        if(this.props.is_login)
+            return (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/calculator/calculator">Calculator</Link>
+                </li>
+            );
+        else
+            return "";
+    }
+
+    render_user = () =>{
+        if(this.props.is_login)
+            return (
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <div className="nav-link" >{this.props.username}</div>
+                    </li>
+                    <li className="nav-item">
+                        <a onClick={this.handleClick} className="nav-link" to="/calculator/register">Logout</a>
+                    </li>
+                </ul>
+            );
+        else
+            return (
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link active" to="/calculator/login">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/calculator/register">Register</Link>
+                    </li>
+                </ul>
+            )
+    }
+
     render() { 
         return (
             <React.Fragment>
@@ -12,23 +61,14 @@ class NavBar extends Component {
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                          </button>
-                        <div className="collapse navbar-collapse" id="navbarText">
+                         <div className="collapse navbar-collapse" id="navbarText">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
                                     <Link className="nav-link active" to="/calculator/home">Home</Link>
                                 </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/calculator/calculator">Calculator</Link>
-                                </li>
+                                {this.render_calculator()}
                             </ul>
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" to="/calculator/login">Login</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/calculator/register">Register</Link>
-                                </li>
-                            </ul>
+                            {this.render_user()}
                         </div>
                     </div>
                 </nav>
@@ -36,5 +76,6 @@ class NavBar extends Component {
         );
     }
 }
+
  
 export default NavBar;
